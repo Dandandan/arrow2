426f3d23a215ba8632d14105b72349e984d4e283
Heres, Daniel <danielheres@gmail.com>
2021-04-23 09:30:17 +0200

    Speed up null_sum

--- src/compute/aggregate/mod.rs
+++ src/compute/aggregate/mod.rs
@@ -216,11 +216,9 @@ fn null_sum<T: NativeType + AddAssign + Sum>(values: &[T], bitmap: &Bitmap) -> T
         .zip(iter)
         .fold(T::new_simd(), |mut acc, (chunk, validity_chunk)| {
             let chunk = T::from_slice(chunk);
-            let mut iter = BitChunkIter::new(validity_chunk);
-            for i in 0..T::LANES {
-                if iter.next().unwrap() {
-                    acc[i] += chunk[i];
-                }
+            let iter = BitChunkIter::new(validity_chunk);
+            for (i, b) in (0..T::LANES).zip(iter) {
+                acc[i] += if b { chunk[i] } else { T::default() };
             }
             acc
         });
@@ -236,6 +234,7 @@ fn null_sum<T: NativeType + AddAssign + Sum>(values: &[T], bitmap: &Bitmap) -> T
     }
     reduced
 }
+
 
 /// Returns the sum of values in the array.
 ///
